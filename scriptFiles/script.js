44eb8b7b45ed19bae62d975e3c1117d4a84ceb2d
LMGurney <23641941+LMGurney@users.noreply.github.com>
2023-09-26 19:38:45 -0400

    Starter files for in-class activity

---
+++ scriptFiles/script.js
@@ -0,0 +1,47 @@
+
+
+
+/// new js above /////
+function hideElement(){
+	var x = document.getElementById("hideMe");
+	if (x.style.display === 'none'){
+		x.style.display = 'block';
+	}else{
+		x.style.display = 'none';
+	}
+}
+
+function swapImg(myCupCake){
+	myCupCake.src="images/rainbowChocolate1.png";
+}
+
+function swapImgBack(myCupCake){
+	myCupCake.src="images/rainbowStrawberry.png";
+}
+
+function MouseRollover(myImg){
+	myImg.src="images/cupcakes-2285209_1280.jpg";
+}
+
+function MouseOut(myImg){
+	myImg.src="images/cupcakesImage.jpg";
+}
+
+function validateForm(){
+	let fname = document.forms['myForm']['fname'].value;
+	if (fname === ""){
+		alert("Name must be filled out");
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
